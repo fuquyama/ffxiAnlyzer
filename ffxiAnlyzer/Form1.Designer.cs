a8bb241c46289dcd29ba1069d6e7e772e33642aa
fuquyama <fuquyama@gmail.com>
2016-04-06 09:01:43 +0000

    プロジェクト ファイルを追加します。

---
+++ ffxiAnlyzer/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace ffxiAnlyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonTest = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // buttonTest
+            // 
+            this.buttonTest.Location = new System.Drawing.Point(101, 123);
+            this.buttonTest.Name = "buttonTest";
+            this.buttonTest.Size = new System.Drawing.Size(75, 23);
+            this.buttonTest.TabIndex = 0;
+            this.buttonTest.Text = "button1";
+            this.buttonTest.UseVisualStyleBackColor = true;
+            this.buttonTest.Click += new System.EventHandler(this.buttonTest_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.buttonTest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonTest;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ ffxiAnlyzer/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using HtmlAgilityPack;
+
+namespace ffxiAnlyzer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonTest_Click(object sender, EventArgs e)
+        {
+            //if (openFileDialog1.ShowDialog() != DialogResult.OK)
+            //{
+            //    return;
+            //}
+
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(@"<font color=""#808480""><b><!--6a,00,00,80808480,000050ed,00005faf,000f,00,01,02,00,00000000,00000000,00000000,00000000,00000000,00000000,00000000,00000000,00000000,00--> Jajaの震天動地の章！</b></font><br>");
+            var nodes = doc.DocumentNode.SelectNodes("//b");
+
+            foreach (var b in nodes)
+            {
+                string str = b.LastChild.InnerText;
+            }
+        }
+    }
+}
